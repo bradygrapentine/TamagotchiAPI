0f1316c8ce78b8f96f205a95729881c9fe23a7f7
bradygrapentine <brady.grapentine@gmail.com>
2021-05-06 18:26:11 -0400

    Filling out controller methods

---
+++ Models/Pet.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+namespace TamagotchiAPI.Models
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public int HungerLevel { get; set; }
+        public int HappinessLevel { get; set; }
+        public DateTime LastInteractedWithDate { get; set; }
+        public List<Playtime> PetPlaytimes { get; set; }
+        public List<Feeding> PetFeedings { get; set; }
+        public List<Scolding> PetScoldings { get; set; }
+        public Boolean IsDead
+        {
+            get
+            {
+                if (LastInteractedWithDate.AddDays(3) >= DateTime.Now)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
